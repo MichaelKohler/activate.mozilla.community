e1f57e40825ed3c47546b5cf201a3b8c6093ca73
Michael Kohler <me@michaelkohler.info>
2017-02-20 19:39:47 +0100

    Add missing files

---
+++ updateMDFromXLF.sh
@@ -0,0 +1,35 @@
+# pt-BR
+node_modules/md2xliff/bin/reconstruct locales/pt-BR/activities.xlf locales/pt-BR/activities.skl.md _pages/pt/activities.md
+node_modules/md2xliff/bin/reconstruct locales/pt-BR/e10s-addons.xlf locales/pt-BR/e10s-addons.skl.md _pages/pt/e10s-addons.md
+node_modules/md2xliff/bin/reconstruct locales/pt-BR/faq.xlf locales/pt-BR/faq.skl.md _pages/pt/faq.md
+node_modules/md2xliff/bin/reconstruct locales/pt-BR/leaderboard.xlf locales/pt-BR/leaderboard.skl.md _pages/pt/leaderboard.md
+node_modules/md2xliff/bin/reconstruct locales/pt-BR/participation.xlf locales/pt-BR/participation.skl.md _pages/pt/participation.md
+node_modules/md2xliff/bin/reconstruct locales/pt-BR/roadmap.xlf locales/pt-BR/roadmap.skl.md _pages/pt/roadmap.md
+node_modules/md2xliff/bin/reconstruct locales/pt-BR/rust-hack.xlf locales/pt-BR/rust-hack.skl.md _pages/pt/rust-hack.md
+node_modules/md2xliff/bin/reconstruct locales/pt-BR/test-pilot.xlf locales/pt-BR/test-pilot.skl.md _pages/pt/test-pilot.md
+node_modules/md2xliff/bin/reconstruct locales/pt-BR/webcompat-sprint.xlf locales/pt-BR/webcompat-sprint.skl.md _pages/pt/webcompat-sprint.md
+node_modules/md2xliff/bin/reconstruct locales/pt-BR/webvr-camp.xlf locales/pt-BR/webvr-camp.skl.md _pages/pt/webvr-camp.md
+
+# es-ES
+node_modules/md2xliff/bin/reconstruct locales/es-ES/activities.xlf locales/es-ES/activities.skl.md _pages/es/activities.md
+node_modules/md2xliff/bin/reconstruct locales/es-ES/e10s-addons.xlf locales/es-ES/e10s-addons.skl.md _pages/es/e10s-addons.md
+node_modules/md2xliff/bin/reconstruct locales/es-ES/faq.xlf locales/es-ES/faq.skl.md _pages/es/faq.md
+node_modules/md2xliff/bin/reconstruct locales/es-ES/leaderboard.xlf locales/es-ES/leaderboard.skl.md _pages/es/leaderboard.md
+node_modules/md2xliff/bin/reconstruct locales/es-ES/participation.xlf locales/es-ES/participation.skl.md _pages/es/participation.md
+node_modules/md2xliff/bin/reconstruct locales/es-ES/roadmap.xlf locales/es-ES/roadmap.skl.md _pages/es/roadmap.md
+node_modules/md2xliff/bin/reconstruct locales/es-ES/rust-hack.xlf locales/es-ES/rust-hack.skl.md _pages/es/rust-hack.md
+node_modules/md2xliff/bin/reconstruct locales/es-ES/test-pilot.xlf locales/es-ES/test-pilot.skl.md _pages/es/test-pilot.md
+node_modules/md2xliff/bin/reconstruct locales/es-ES/webcompat-sprint.xlf locales/es-ES/webcompat-sprint.skl.md _pages/es/webcompat-sprint.md
+node_modules/md2xliff/bin/reconstruct locales/es-ES/webvr-camp.xlf locales/es-ES/webvr-camp.skl.md _pages/es/webvr-camp.md
+
+# fr
+node_modules/md2xliff/bin/reconstruct locales/fr/activities.xlf locales/fr/activities.skl.md _pages/fr/activities.md
+node_modules/md2xliff/bin/reconstruct locales/fr/e10s-addons.xlf locales/fr/e10s-addons.skl.md _pages/fr/e10s-addons.md
+node_modules/md2xliff/bin/reconstruct locales/fr/faq.xlf locales/fr/faq.skl.md _pages/fr/faq.md
+node_modules/md2xliff/bin/reconstruct locales/fr/leaderboard.xlf locales/fr/leaderboard.skl.md _pages/fr/leaderboard.md
+node_modules/md2xliff/bin/reconstruct locales/fr/participation.xlf locales/fr/participation.skl.md _pages/fr/participation.md
+node_modules/md2xliff/bin/reconstruct locales/fr/roadmap.xlf locales/fr/roadmap.skl.md _pages/fr/roadmap.md
+node_modules/md2xliff/bin/reconstruct locales/fr/rust-hack.xlf locales/fr/rust-hack.skl.md _pages/fr/rust-hack.md
+node_modules/md2xliff/bin/reconstruct locales/fr/test-pilot.xlf locales/fr/test-pilot.skl.md _pages/fr/test-pilot.md
+node_modules/md2xliff/bin/reconstruct locales/fr/webcompat-sprint.xlf locales/fr/webcompat-sprint.skl.md _pages/fr/webcompat-sprint.md
+node_modules/md2xliff/bin/reconstruct locales/fr/webvr-camp.xlf locales/fr/webvr-camp.skl.md _pages/fr/webvr-camp.md
---
+++ updateXLFFromMD.sh
@@ -0,0 +1,23 @@
+#en-US
+node_modules/md2xliff/bin/extract _pages/activities.md locales/en-US/activities.xlf locales/en-US/activities.skl.md en-US en-US
+node_modules/md2xliff/bin/extract _pages/e10s-addons.md locales/en-US/e10s-addons.xlf locales/en-US/e10s-addons.skl.md en-US en-US
+node_modules/md2xliff/bin/extract _pages/faq.md locales/en-US/faq.xlf locales/en-US/faq.skl.md en-US en-US
+node_modules/md2xliff/bin/extract _pages/leaderboard.md locales/en-US/leaderboard.xlf locales/en-US/leaderboard.skl.md en-US en-US
+node_modules/md2xliff/bin/extract _pages/participation.md locales/en-US/participation.xlf locales/en-US/participation.skl.md en-US en-US
+node_modules/md2xliff/bin/extract _pages/roadmap.md locales/en-US/roadmap.xlf locales/en-US/roadmap.skl.md en-US en-US
+node_modules/md2xliff/bin/extract _pages/rust-hack.md locales/en-US/rust-hack.xlf locales/en-US/rust-hack.skl.md en-US en-US
+node_modules/md2xliff/bin/extract _pages/test-pilot.md locales/en-US/test-pilot.xlf locales/en-US/test-pilot.skl.md en-US en-US
+node_modules/md2xliff/bin/extract _pages/webcompat-sprint.md locales/en-US/webcompat-sprint.xlf locales/en-US/webcompat-sprint.skl.md en-US en-US
+node_modules/md2xliff/bin/extract _pages/webvr-camp.md locales/en-US/webvr-camp.xlf locales/en-US/webvr-camp.skl.md en-US en-US
+
+# pt-BR
+node_modules/md2xliff/bin/extract _pages/pt/activities.md locales/pt-BR/activities.xlf locales/pt-BR/activities.skl.md en-US pt-BR
+node_modules/md2xliff/bin/extract _pages/pt/e10s-addons.md locales/pt-BR/e10s-addons.xlf locales/pt-BR/e10s-addons.skl.md en-US pt-BR
+node_modules/md2xliff/bin/extract _pages/pt/faq.md locales/pt-BR/faq.xlf locales/pt-BR/faq.skl.md en-US pt-BR
+node_modules/md2xliff/bin/extract _pages/pt/leaderboard.md locales/pt-BR/leaderboard.xlf locales/pt-BR/leaderboard.skl.md en-US pt-BR
+node_modules/md2xliff/bin/extract _pages/pt/participation.md locales/pt-BR/participation.xlf locales/pt-BR/participation.skl.md en-US pt-BR
+node_modules/md2xliff/bin/extract _pages/pt/roadmap.md locales/pt-BR/roadmap.xlf locales/pt-BR/roadmap.skl.md en-US pt-BR
+node_modules/md2xliff/bin/extract _pages/pt/rust-hack.md locales/pt-BR/rust-hack.xlf locales/pt-BR/rust-hack.skl.md en-US pt-BR
+node_modules/md2xliff/bin/extract _pages/pt/test-pilot.md locales/pt-BR/test-pilot.xlf locales/pt-BR/test-pilot.skl.md en-US pt-BR
+node_modules/md2xliff/bin/extract _pages/pt/webcompat-sprint.md locales/pt-BR/webcompat-sprint.xlf locales/pt-BR/webcompat-sprint.skl.md en-US pt-BR
+node_modules/md2xliff/bin/extract _pages/pt/webvr-camp.md locales/pt-BR/webvr-camp.xlf locales/pt-BR/webvr-camp.skl.md en-US pt-BR
