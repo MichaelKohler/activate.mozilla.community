a7a51b0df00fda8b9548c304e0f75ba73e6647b1
Michael Kohler <me@michaelkohler.info>
2018-06-30 12:49:57 +0200

    Add moment.js again..

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,19 +20,10 @@ $(document).ready(function() {
     });
     show_back();
 
-    // scroll body to 0px on click
     $('#back-top a').click(function () {
         $('body,html').animate({
             scrollTop: 0
         }, 800);
         return false;
-    });
-
-    $('.frontpage-slider').bxSlider({
-      auto: true,
-      controls: true,
-      pager: false,
-      pause: 6000,
-      speed: 800
     });
 });
